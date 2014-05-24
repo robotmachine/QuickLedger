e9f18fe7b77b7e9d2efec8b19b72f21c93178bf0
Brian Carter <robotmachine@gmail.com>
2014-05-24 15:14:14 -0700

    Checks on system environment variable for LEDGER and LEDGER_FILE.

--- ql.py
+++ ql.py
@@ -14,7 +14,7 @@
 * To Public License, Version 2, as published by Sam Hocevar. See
 * http://sam.zoy.org/wtfpl/COPYING for more details.
 """
-import os, sys, datetime, argparse, configparser
+import os, sys, textwrap, datetime, argparse, configparser
 
 settings = os.path.expanduser("~/.qlrc")
 config = configparser.ConfigParser()
@@ -27,6 +27,38 @@ def main():
 	args = parser.parse_args()
 	read_config(file=args.file)
 
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":True,   "y":True,  "ye":True,
+             "no":False,     "n":False}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
 def read_config(file):
 	if file is None:
 		if os.path.exists(settings):
@@ -37,9 +69,22 @@ def read_config(file):
 		datesel(file)
 
 def set_config():
-	file = None
+	if os.environ['LEDGER']:
+		system_ledger = str(os.environ['LEDGER'])
+	elif os.environ['LEDGER_FILE']:
+		system_ledger = str(os.environ['LEDGER_FILE'])
+	if system_ledger:
+		print(textwrap.dedent("""
+		Looks like your default ledger file is
+		%s
+		Use that for ql?	
+		""") % (system_ledger))
+		file = system_ledger
+	else:
+		file = input("Ledger file location: ")
 	datesel(file)
 		
+
 def datesel(file):
 	tdateraw = []
 	today = datetime.date.today()
