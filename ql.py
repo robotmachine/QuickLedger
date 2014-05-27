027d2ddc0a912b5988f1872ab0410128d0a534f7
Brian Carter <robotmachine@gmail.com>
2014-05-26 19:12:49 -0700

    Removed unneeded function.

--- ql.py
+++ ql.py
@@ -27,30 +27,9 @@ def main():
 	args = parser.parse_args()
 	read_config(file=args.file)
 
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is one of "yes" or "no".
-    """
-    valid = {"yes":True,   "y":True,  "ye":True,
-             "no":False,     "n":False}
-    if default == None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
     while True:
         sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
+        choice = input().lower()
         if default is not None and choice == '':
             return valid[default]
         elif choice in valid:
@@ -77,13 +56,11 @@ def set_config():
 		print(textwrap.dedent("""
 		Looks like your default ledger file is
 		%s
-		Use that for ql?	
 		""") % (system_ledger))
 		file = system_ledger
 	else:
 		file = input("Ledger file location: ")
 	datesel(file)
-		
 
 def datesel(file):
 	tdateraw = []
