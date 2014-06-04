3874e96d0d2e2a7ebf0e34773976823a812eb103
Brian Carter <robotmachine@gmail.com>
2014-06-03 20:59:26 -0700

    Added arg for account and nicknamed account. Updated README to document layout of qlrc. Resolves #7.

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # ql
 
-### Quick entry creation tool for [Ledger](http://ledger-cli.org/ "Ledger").  
+## Quick entry creation tool for [Ledger](http://ledger-cli.org/ "Ledger").  
 
->usage: ql [-h] [-f LEDGER_FILE]  
->  
+### Usage
+>usage: ql [-h] [-f LEDGER_FILE] [-a ACCOUNT] [-n NICKNAME]  
+>    
 >ql: Quick `ledger' entry creation tool.  
 >  
 >optional arguments:  
->  -h, --help      show this help message and exit  
->  -f LEDGER_FILE  Specify Ledger file.  
+>  -h, --help            show this help message and exit  
+>  -f LEDGER_FILE, --file LEDGER_FILE  
+>                        Specify Ledger file.  
+>  -a ACCOUNT, --account ACCOUNT  
+>                        Specify account.  
+>  -n NICKNAME, --nick NICKNAME  
+>                        Specify account from qlrc by nickname.  
+
+### Example qlrc file
+
+>[ql]  
+>ledger_file = /home/robotmachine/Doc/Ledger/Ledger.dat  
+>default = BANK  
+>BANK = Assets:Bank:Personal
+>SAVE = Assets:Bank:Savings  
+  
+`ql` with no arguments for account or nickname will read the account listed as default. Running `ql --nick SAVE` will use the account nicknamed SAVE from qlrc.
--- ql.py
+++ ql.py
@@ -27,42 +27,62 @@ def main():
 	Reads command line arguments to determine if a file is specified.		
 	"""
 	parser = argparse.ArgumentParser(description="ql: Quick `ledger' entry creation tool.", prog='ql')
-	parser.add_argument('-f',
+	parser.add_argument('-f', '--file',
 		action='store', dest='ledger_file', default=None,
 		help='Specify Ledger file.')
+	parser.add_argument('-a', '--account',
+		action='store', dest='account', default=None,
+		help='Specify account.')
+	parser.add_argument('-n', '--nick',
+		action='store', dest='nickname', default=None,
+		help='Specify account from qlrc by nickname.')
 	args = parser.parse_args()
-	read_config(ledger_file=args.ledger_file)
+	read_config(ledger_file=args.ledger_file, account=args.account, nick=args.nickname)
 
-def read_config(ledger_file):
+def read_config(ledger_file, account, nick):
 	"""
 	If a settings file is specified on the command line, then just skip to the 
 	actual ledger entry. If not, ql checks for the file. If it is there, then 
 	ql reads from it. If there is no settings file and no file was specified, then
 	we move to set up the config file.	
 	"""
-	if ledger_file is None:
-		if os.path.exists(settings):
-			config.read(settings)
-			ledger_file = config['ql']['ledger_file']
-			if os.path.isfile(ledger_file):
-				datesel(ledger_file)
-			else:
-				print("Error! Cannot find %s" % ledger_file)
-		else:
-			set_config()
-	else:
+	if os.path.exists(settings):
+		config.read(settings)
+		if ledger_file is None:
+			try:
+				ledger_file = config['ql']['ledger_file']
+			except:
+				ledger_file = None
+		if account is None:
+			try:
+				default_account = config['ql']['default']
+			except:
+				default_account = None
+			if default_account is not None:
+				try:
+					account = config['ql'][default_account]
+				except:
+					account = None
+		if nick is not None:
+			try:
+				account = config['ql'][nick]
+			except:
+				print("No account called %s found in .qlrc" % nick)
+			
 		if os.path.isfile(ledger_file):
-			datesel(ledger_file)
+			datesel(ledger_file, account)
 		else:
 			print("Error! Cannot find %s" % ledger_file)
-			quit()
-
-def set_config():
-	if os.environ['LEDGER']:
-		system_ledger = os.path.expanduser(os.environ['LEDGER'])
-	elif os.environ['LEDGER_FILE']:
-		system_ledger = os.path.expanduser(os.environ['LEDGER_FILE'])
 	else:
+		set_config(account)
+
+def set_config(account):
+	try:
+		if os.environ['LEDGER']:
+			system_ledger = os.path.expanduser(os.environ['LEDGER'])
+		elif os.environ['LEDGER_FILE']:
+			system_ledger = os.path.expanduser(os.environ['LEDGER_FILE'])
+	except:
 		system_ledger = None
 	if system_ledger is not None: 
 		print(textwrap.dedent("""
@@ -70,52 +90,65 @@ def set_config():
 		%s
 		""") % (system_ledger))
 		query = input("Use this file for ql? [y/n] ")
-		boolquery = distutils.util.strtobool(query)
+		try:
+			boolquery = distutils.util.strtobool(query)
+		except:
+			print("Must enter yes or no.")
+			set_config(account)
 		if boolquery == True:
 			led_file = system_ledger
 		else:
-			led_input = input("Ledger file location: ")
-			led_file = os.path.expanduser(led_input)	
-			if not os.path.isfile(led_file):
-				print("File not found.")
-				quit()	
+			led_file = None
+	else:
+		led_file = None
+
+	if led_file is None:
+		led_input = input("Ledger file location: ")
+		led_file = os.path.expanduser(led_input)	
+		if not os.path.isfile(led_file):
+			print("File not found.")
+			quit()	
+	
 	if os.path.isfile(led_file):
 		config ['ql'] = {'ledger_file': led_file}
 		with open(settings, 'w') as configfile:
 			config.write(configfile)
-		read_config(led_file)
+		read_config(led_file, account)
 
-def datesel(ledger_file):
+def datesel(ledger_file, account):
 	tdateraw = []
 	today = datetime.date.today()
 	tdateraw.append(today)
 	tdate = str(tdateraw[0])
-	merchsel(ledger_file, tdate)
+	merchsel(ledger_file, account, tdate)
 
-def merchsel(ledger_file, tdate):
+def merchsel(ledger_file, account, tdate):
 	try:
-		merchant = str(input("Merchant name: "))
-		category = str(input("Expense category: "))
+		merchant = str(input("Merchant name:\n\t"))
+		category = str("Expenses:")+str(input("Expense category:\n\tExpenses:"))
+		if account is None:
+			account = str("Assets:")+str(input("Account:\n\tAssets:"))
 	except:
 		print("Syntax error.")
-		merchsel(ledger_file, tdate)
-	amountsel(ledger_file, tdate, merchant, category)
+		merchsel(ledger_file, account, tdate)
+	amountsel(ledger_file, tdate, merchant, category, account)
 
-def amountsel(ledger_file, tdate, merchant, category):
+def amountsel(ledger_file, tdate, merchant, category, account):
 	try:
 		amount_dec = Decimal(input("Amount: $")).quantize(Decimal('1.00'))
 	except: 
 		print("Amount must be a number.")
-		amountsel(ledger_file, tdate, merchant, category)
+		amountsel(ledger_file, tdate, merchant, category, account)
 	amount = str(amount_dec)
-	printer(ledger_file, tdate, merchant, category, amount)
+	printer(ledger_file, tdate, merchant, category, account, amount)
 
-def printer(ledger_file, tdate, merchant, category, amount):
+def printer(ledger_file, tdate, merchant, category, account, amount):
+	ledger_entry = tdate+" * "+merchant+"\n\t"+category+"\t\t$"+amount+"\n\t"+account+"\n"
 	try:
 		with open(ledger_file, "a") as ledger_write:
-			ledger_write.write(tdate+" * "+merchant+"\n\tExpenses:"+category+"\t\t$"+amount+"\n\tAssets:OSU:Brian"+"\n")
+			ledger_write.write(ledger_entry)
 			ledger_write.close()
-			print("Wrote entry:\n"+tdate+" * "+merchant+"\n\tExpenses:"+category+"\t\t$"+amount+"\n\tAssets:OSU:Brian")
+			print("\n\nWrote entry to "+ledger_file+":\n\n"+ledger_entry)
 	except PermissionError:
 		print("Cannot write to %s. Permission error!" % ledger_file)
 	quit()
