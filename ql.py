7cae803cf478334026d3c904da2879eb032e19f4
Brian Carter <robotmachine@gmail.com>
2014-11-30 18:09:19 -0800

    Fixed #12 and this is where bool_tool() and query_tool() were created.

--- ql.py
+++ ql.py
@@ -26,7 +26,12 @@ import os, sys, textwrap, datetime, argparse, configparser, distutils.util
 from decimal import *
 
 """ Settings are stored in .qlrc in user's home folder. """
-settings = os.path.expanduser("~/.qlrc")
+global settings
+setpath = os.path.expanduser("~/.qlrc")
+if os.path.exists(setpath):
+	settings = setpath
+
+global config
 config = configparser.ConfigParser()
 
 def main():
@@ -59,7 +64,7 @@ def main():
 		action='store_true', dest='setup',
 		help='Set up accounts and merchants in config file.')
 	parser.add_argument('--config',
-		action='store', dest='set_config', default=None,
+		action='store', dest='alt_config', default=None,
 		help='Specify alternate config file.')
 	args = parser.parse_args()
 
@@ -69,17 +74,21 @@ def main():
 	else:
 		clrstat = "*"
 
-	if args.setup:
-		setup()
-
-	global set_config
-	if args.set_config is not None:
-		try:
-			set_config = os.path.expanduser(args.set_config)
-		except:
-			set_config = set_config
+	global settings
+	if args.alt_config is not None:
+		if '~' in args.alt_config:
+			try:
+				expset = os.path.expanduser(args.alt_config)
+				if os.path.exists(expset):
+					settings = expset
+			except:
+				if os.path.exists(args.alt_config):
+					settings = args.alt_config
+		else:
+			if os.path.exists(args.alt_config):
+				settings = args.alt_config
 	else:
-		set_config = None
+		alt_config = None
 
 	category = args.category
 	if args.expense:
@@ -88,6 +97,8 @@ def main():
 	amount = args.amount
 	account = args.account
 	merchant = args.merchant
+	if args.setup:
+		setup()
 	read_config(ledger_file, account, merchant, category, amount)
 
 def read_config(ledger_file, account, merchant, category, amount):
@@ -97,13 +108,12 @@ def read_config(ledger_file, account, merchant, category, amount):
 	ql reads from it. If there is no settings file and no file was specified, then
 	we move to set up the config file.	
 	"""
-	if set_config is not None:
-		if os.path.exists(set_config):
-			config.read(set_config)
-	elif os.path.exists(settings):
+
+	if os.path.exists(settings):
 		config.read(settings)
 	else:
 		set_config(account, merchant, category, amount)
+
 	if ledger_file is None:
 		try:
 			ledger_file = config['file']['ledger_file']
@@ -155,12 +165,7 @@ def set_config(account, merchant, category, amount):
 		Looks like your default ledger file is
 		%s
 		""") % (system_ledger))
-		query = input("Use this file for ql? [y/n] ")
-		try:
-			boolquery = distutils.util.strtobool(query)
-		except:
-			print("Must enter yes or no.")
-			set_config(account)
+		boolquery = bool_tool("Use this file for ql? [y/N] ")
 		if boolquery == True:
 			led_file = system_ledger
 		else:
@@ -173,10 +178,10 @@ def set_config(account, merchant, category, amount):
 	If the file is not found, an error will print.
 	"""
 	if led_file is None:
-		led_input = input("Ledger file location: ")
+		led_input = query_tool('Ledger file location: ')
 		led_file = os.path.expanduser(led_input)	
 		if not os.path.isfile(led_file):
-			print("File not found.")
+			print('File not found.')
 			quit()	
 	"""
 	Checks again to make sure the ledger_file actually exists.
@@ -189,28 +194,48 @@ def set_config(account, merchant, category, amount):
 		read_config(led_file, account, merchant, category, amount)
 
 def setup():
-	query = input("Would you like to add an account to the ql config file? [Y/n] ")
-	try:
-		boolquery = distutils.util.strtobool(query)
-	except:
-		print("Must enter yes or no.")
-		setup()
+	boolquery = bool_tool("\nWould you like to add an account to the ql config file? [y/N] ")
 	if boolquery == True:
 		accounts()
 	else:
-		query = input("Would you like to add merchants to the ql config file? [Y/n] ")
-		try:
-			boolquery = distutils.util.strtobool(query)
-		except:
-			print("Must enter yes or no.")
-			setup()
+		boolquery = bool_tool("Would you like to add merchants to the ql config file? [y/N] ")
 		if boolquery == True:
 			merchants()
 		else:
 			quit()
 
 def accounts():
-	print("You are a star.")
+	shortname = query_tool('\nEnter a short name for the account: ')
+	acctname = 'Assets:'+query_tool('\nEnter the account name:  Assets:')
+	fullacct = shortname+" = "+acctname
+	if os.path.exists(settings):
+		config.read(settings)
+	else:
+		print('No .qlrc file found. Creating ~/.qlrc')
+
+	try:
+		defaultq = config['acct']['default_account']
+	except:
+		defaultq = None
+	if defaultq is None:
+		boolquery = bool_tool("\nWould you like to set this as the default account? [y/N]: ")
+		if boolquery == True:
+			makedefault = True
+		else:
+			makedefault = False
+	elif defaultq is not None:
+		defaultqq = config['acct'][defaultq]
+		boolquery = bool_tool("\nThe current default account is "+defaultqq+"\nWould you like to replace it with this one? [Y/n] ")
+		if boolquery == True:
+			makedefault = True
+		else:
+			makedefault = False
+
+	config.set('acct',shortname,acctname)
+	if makedefault == True:
+		config.set('acct','default_account',shortname)
+	with open(settings, 'w') as configfile:
+		config.write(configfile)
 	quit()
 
 def merchants():
@@ -225,50 +250,32 @@ def datesel(ledger_file, account, merchant, category, amount):
 	merchsel(ledger_file, account, merchant, category, amount, tdate)
 
 def merchsel(ledger_file, account, merchant, category, amount, tdate):
+
 	if merchant is None:
-		try:
-			merchant = str(input("Merchant name:\n\t"))
-		except KeyboardInterrupt:
-			user_exit()
-		except:
-			print("Syntax error.")
-			merchsel(ledger_file, account, merchant, tdate)
+		merchant = str(query_tool("Merchant name:\n\t"))
 	else:
 		merchant = str(merchant)
+
 	if category is None:
-		try:
-			category = str("Expenses:")+str(input("Expense category:\n\tExpenses:"))
-		except KeyboardInterrupt:
-			user_exit()
-		except:
-			print("Syntax error.")
-			merchsel(ledger_file, account, merchant, tdate)
+		category = str("Expenses:")+str(query_tool("Expense category:\n\tExpenses:"))
 	else:
 		category = str(category)
+
 	if account is None:
-		try:
-			account = str("Assets:")+str(input("Account:\n\tAssets:"))
-		except KeyboardInterrupt:
-			user_exit()
-		except:
-			print("Syntax error.")
-			merchsel(ledger_file, account, merchant, tdate)
+		account = str("Assets:")+str(query_tool("Account:\n\tAssets:"))
+	else:
+		account = str(account)
+
 	amountsel(ledger_file, tdate, merchant, category, amount, account)
 
 def amountsel(ledger_file, tdate, merchant, category, amount, account):
 	if amount is None:
-		try:
-			amount_dec = Decimal(input("Amount: $")).quantize(Decimal('1.00'))
-		except KeyboardInterrupt:
-			user_exit()
-		except: 
-			print("Amount must be a number.")
-			amountsel(ledger_file, tdate, merchant, category, amount, account)
+		amount_dec = Decimal(query_tool('Amount: $')).quantize(Decimal('1.00'))
 	elif amount is not None:
 		try:
 			amount_dec = Decimal(amount).quantize(Decimal('1.00'))
 		except:
-			print("Amount must be a number.")
+			print('Amount must be a number.')
 			amount = None
 			amountsel(ledger_file, tdate, merchant, category, amount, account)
 	amount = str(amount_dec)
@@ -284,7 +291,27 @@ def printer(ledger_file, tdate, merchant, category, account, amount):
 	except PermissionError:
 		print("Cannot write to %s. Permission error!" % ledger_file)
 	quit()
+
 def user_exit():
 	print("\nUser exited.")
 	quit()
+
+def query_tool(query):
+	try:
+		result = input(query)
+		return result
+	except KeyboardInterrupt:
+		user_exit()
+	except:
+		print("\nSyntax error.")
+		quit()
+
+def bool_tool(query):
+	bquery = query_tool(query)
+	try:
+		result = bool(distutils.util.strtobool(bquery))
+		return result
+	except:
+		result = False
+		return result
 main()
